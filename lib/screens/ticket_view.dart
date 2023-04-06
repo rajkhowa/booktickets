5c7ebc615b582b29f7b5a71ec4c8d9b21f7a9f3e
rajkhowa <jrajkhowa28@gmail.com>
2023-04-06 10:21:46 +0530

    day6

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -38,7 +38,45 @@ class TicketView extends StatelessWidget {
                         ),
                         Expanded(child: Container()),
                         ThickContainer(),
-                        Expanded(child: Container()),
+                        Expanded(
+                          child: Stack(children: [
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                    BoxConstraints constraints) {
+                                  print(
+                                      "The width is ${constraints.constrainWidth()}");
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: List.generate(
+                                      (constraints.constrainWidth() / 6)
+                                          .floor(),
+                                      (index) => const SizedBox(
+                                        width: 3,
+                                        height: 1,
+                                        child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white)),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Center(
+                              child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ]),
+                        ),
                         ThickContainer(),
                         Expanded(child: Container()),
                         Text(
